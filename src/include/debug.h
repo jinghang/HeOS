aeeff00101fc332bc6f14f45cf3a650c0ace32c3
jinghang <jhang.cn@gmail.com>
2018-12-11 14:13:22 +0800

    增加内核调式打印

--- src/include/debug.h
+++ src/include/debug.h
@@ -1,8 +1,35 @@
+
+#ifndef INCLUDE_DEBUG_H
+#define INCLUDE_DEBUG_H
+
 #include "console.h"
 #include "vargs.h"
+#include "elf.h"
+
+#define assert(x, info)       \
+    do{                       \
+        if(!(x)){             \
+            panic(info);      \
+        }                     \
+    }while(0)
+
+// 编译期间静态检测
+#define static _assert(x) switch(x){case 0: case (x): ;}
+
+// 初始化 debug 信息
+void init_debug();
+
+// 打印当前的函数调用栈
+void panic(const char *msg);
+
+// 打印当前的段寄存器的值
+void print_cur_status();
+
 
 // 内和打印函数
 void printk(const char *format, ...);
 
 // 内核打印函数，带颜色
 void printk_color(console_background_color_t back, console_foreground_color_t fore, const char *format, ...);
+
+#endif // INCLUDE_DEBUG_H
