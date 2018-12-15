359879415b1133022fdd3e970d67469315dd6be7
jinghang <jhang.cn@gmail.com>
2018-12-16 01:04:40 +0800

    增加中断描述符

--- src/init/entry.c
+++ src/init/entry.c
@@ -2,15 +2,21 @@
 #include "../include/gdt.h"
 #include "../include/console.h"
 #include "../include/debug.h"
+#include "../include/idt.h"
 
 int kernel_entry(){
     
     init_debug();
     init_gdt();
+    init_idt();
     
     console_clear();
     printk_color(background_color_black, foreground_color_green, "Hello HeOS kernel!\n");
-    panic("test");
+    //panic("test");
+
+    asm volatile("int $0x03");
+    asm volatile("int $0x04");
+    asm volatile("int $0xff");
     
     return 0;
 }
