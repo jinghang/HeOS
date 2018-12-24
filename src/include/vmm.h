be04321eead53fcab37792a0c599d23a604ab2bb
ljh <jhang.cn@gmail.com>
2018-12-24 21:33:54 +0800

    虚拟地址头文件

--- src/include/vmm.h
+++ src/include/vmm.h
@@ -35,4 +35,35 @@ typedef uint32_t pgd_t;
 // 页表数据项
 typedef uint32_t pte_t;
 
+// 页表成员数
+#define PGD_SIZE (PAGE_SIZE/sizeof(pte_t))
+
+// 页表成员数
+#define PTE_SIZE (PAGE_SIZE/sizeof(uint32_t))
+
+// 映射 512MB 内存所需的页
+#define PTE_COUNT 128
+
+// 内核页目录区域
+extern pgd_t pgd_kern[PGD_SIZE];
+
+// 初始化虚拟内存管理
+void init_vmm();
+
+// 更换当前页目录
+void switch_pgd(uint32_t pd);
+
+// 把物理地质 pa 映射到虚拟地质 va
+void map(pgd_t * pgd_now, uint32_t va, uint32_t pa, uint32_t flags);
+
+// 取消虚拟地质 va 的物理地质映射
+void unmap(pgd_t *pgd_now, uint32_t va);
+
+// 如果虚拟地址 va 映射到物理地质，则返回 1
+// 同时如果 pa 不是空指针， 则把物理地址写入 pa 参数
+uint32_t get_mapping(pgd_t *pgd_now, uint32_t va, uint32_t *pa);
+
+// 页错误中断的函数处理
+void page_fault(pt_regs *regs);
+
 #endif // INCLUDE_VMM_H
